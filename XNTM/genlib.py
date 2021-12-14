eda833bc32cc8ec378d27e5f04e114d4edef99a1
musuchika <musu.tec@gmail.com>
2021-12-14 23:06:19 +0900

    配置パターンのライブラリ出力

--- XNTM/genlib.py
+++ XNTM/genlib.py
@@ -1,5 +1,8 @@
 import json
 import copy
+import itertools
+import sys
+import datetime
 
 Mixer = ['6h','6v','4']
 Module = ['6h','6v','4','r1','r2','r3','r4','r5']
@@ -85,7 +88,7 @@ def getProvRatio():
         #print(prov_ratio[PM],end="\n"*2)
     return prov_ratio
 
-def CanPlace(placement,condition):
+def CanLeftDropPlace(placement,condition):
     search_cell = copy.deepcopy(placement["overlapping_cell"])
     if "flushing_cell" in placement :
         search_cell += placement["flushing_cell"] 
@@ -95,13 +98,13 @@ def CanPlace(placement,condition):
             return False
     return True
     
-def Place(placement,condition):
+def LeftDropPlace(placement,condition):
     cells = placement["overlapping_cell"]
     new_condition = copy.deepcopy(condition)
 
     for cell in cells:
         if cell not in new_condition[0][0]:
-            print("error : Place()セルが空いてない!!")
+            print("error : LeftDropPlace()セルが空いてない!!")
             return -1
         else :
             new_condition[0][0].remove(cell)
@@ -126,9 +129,9 @@ def genTemplate(pattern):
             for idx,num in enumerate(ratio):
                 for condition in buffer[idx]:
                     drop = "r"+str(num)
-                    for placement in pattern[PM][drop]:
-                        if CanPlace(placement,condition):
-                            next_condition = Place(placement,condition)
+                    for placement in pattern[PM][drop][str(num)]:
+                        if CanLeftDropPlace(placement,condition):
+                            next_condition = LeftDropPlace(placement,condition)
                             #print(condition,placement,next_condition,sep='\n')
                             buffer[idx+1].append(next_condition)
                 #print("\n")
@@ -143,29 +146,102 @@ def genTemplate(pattern):
                     return -1
     return template
 
-def main():
-    Mixer = ['6h','6v','4']
-    Module = ['6h','6v','4','r1','r2','r3','r4','r5']
+def showColoredTemplate(prov_ratio,template):
+    for pm in range(len(Mixer)):
+            PM = Mixer[pm]
+            for ratio in prov_ratio[PM]:
+                cnt = 0
+                for temp in template[PM][str(ratio)]:
+                    cnt +=1
+                    grid = gengrid(PM)
+                    for idx,group in enumerate(temp):
+                        for cell in group :
+                            color = idx+1
+                            y,x = cell
+                            grid[y][x] = color
+                    print(PM,ratio,sep='\t')
+                    showGrid(grid)
+                print(PM,ratio,cnt,sep='\t')
 
-    readfile = open('placement.json','r')
+def search_placement(pattern,PM,module,drop):
+    cnt_drop = len(drop)
+    return pattern[PM][module][str(cnt_drop)]
+    
+def getModule(module):
+    FirstLetter = ['6','4','r']
+    for i in range(len(FirstLetter)):
+        if FirstLetter[i] == module[0]:
+            return FirstLetter[i]
+    return -1
+
+def main():
+    start = datetime.datetime.now()
+    readfile = open('data/placement.json','r')
     pattern = json.load(readfile)
     prov_ratio = getProvRatio()
+    ### generate the combination of placements of dropret left by children-mixers
     template = genTemplate(pattern)
+    ### generate library of placement of children-mixers
+    lib = {}
     for pm in range(len(Mixer)):
         PM = Mixer[pm]
+        lib[PM] = {}
         for ratio in prov_ratio[PM]:
-            cnt = 0
-            for temp in template[PM][str(ratio)]:
-                cnt +=1
-                grid = gengrid(PM)
-                for idx,group in enumerate(temp):
-                    for cell in group :
-                        color = idx+1
-                        y,x = cell
-                        grid[y][x] = color
-                print(PM,ratio,sep='\t')
-                showGrid(grid)
-            print(PM,ratio,cnt,sep='\t')
+            lib[PM][str(ratio)] = []
+            RatioOrderedCombos = template[PM][str(ratio)]
+            for RatioOrderedCombo in RatioOrderedCombos:
+                PermuCombo = list(itertools.permutations(RatioOrderedCombo,len(RatioOrderedCombo)))
+                for combo in PermuCombo:
+                    buffer = [[] for i in range(len(ratio)+1)]
+                    ### [[[配置した6ミキサー],[配置した4ミキサー],[配置した試薬液滴]],[試薬液滴を残せるセル],[レイヤーの確認の為のgrid]]
+                    buffer[0].append({"Module":{"6":[],"4":[],"r":[]},"CannotPlace":[],"Layer":gengrid(PM)})
+                    for idx,DropsLeftByAModule in enumerate(combo):
+                        for buff in buffer[idx]:
+                        ### DropsLeftByAModuleをFlush後に残すモジュールをbuffer[idx]に追加し，buffer[idx+1]に書き込み
+                            for module in range(len(Module)) :
+                                MODULE = Module[module]
+                                if str(len(DropsLeftByAModule)) in pattern[PM][MODULE]:
+                                    for candidate in pattern[PM][MODULE][str(len(DropsLeftByAModule))]:
+                                        if candidate["overlapping_cell"] == DropsLeftByAModule:
+                                            ### check whether the module overlapps droplets left by mixers that placed formerly
+                                            passing = True
+                                            check_cells = candidate["flushing_cell"]
+                                            for check_cell in check_cells:
+                                                if check_cell in buff["CannotPlace"]:
+                                                    passing = False
+                                            if passing :
+                                                new_buff = copy.deepcopy(buff)
+                                                layer = 1
+                                                layer_search_cells = candidate["flushing_cell"]+candidate["overlapping_cell"]
+                                                for layer_search_cell in layer_search_cells:
+                                                    y,x = layer_search_cell
+                                                    layer = max(layer,buff["Layer"][y][x]+1)
+                                                for y,x in layer_search_cells:
+                                                    new_buff["Layer"][y][x] = layer
+                                                for y,x in candidate["overlapping_cell"]:
+                                                    new_buff["CannotPlace"].append([y,x])
+                                                AddedModule = copy.deepcopy(candidate)
+                                                AddedModule["layer"] = layer
+                                                if getModule(MODULE) == '6':
+                                                    AddedModule["orientation"] = MODULE[-1:]
+                                                new_buff["Module"][getModule(MODULE)].append(AddedModule)
+                                                buffer[idx+1].append(new_buff)
+                    if buffer[len(ratio)]:
+                        for data in buffer[len(ratio)]:
+                            del data["Layer"]
+                            del data["CannotPlace"]
+                            lib[PM][str(ratio)].append(data)
+                    else :
+                        print("ホンマ，ありえへんてぇ~!! in genlib.py",file=sys.stderr)
+                    
+
+    ### output lib to json file
+    with open("data/lib.json","w") as f:
+        json.dum(lib,f,indent=4)  
+
+    end = datetime.datetime.now()
+    diff = end-start
+    print("実行時間 = end - start = {} - {} = {}".format(end,start,diff))
 
 if __name__ == '__main__':
     main()
